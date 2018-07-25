e13ac325d32fc649e9e17e7b5e6fd8c7d327177f
ShravaniBajjuri <31980280+ShravaniBajjuri@users.noreply.github.com>
2018-07-25 06:50:19 +0530

    Add files via upload

---
+++ FirstAppDb/src/main/java/com/jdbc/FirstAppDb/JavaSQL1.java
@@ -0,0 +1,28 @@
+package com.jdbc.FirstAppDb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JavaSQL1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    try {
+	    	Class.forName("com.mysql.jdbc.Driver");
+	    	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", "root", "password");
+	    Statement stmnt = con.createStatement();
+	    ResultSet rs = stmnt.executeQuery("select * from category ");
+	    while(rs.next())
+	    	System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getDate(3));
+	       con.close();
+	    }
+	    catch(Exception e)
+	    {
+	    	System.out.println(e);
+	    		}
+
+	}
+
+}
